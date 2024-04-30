285c3afa15278b25e29469c7277389a378a146ce
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-04-30 08:10:17 +0600

    Added Custom Controls

---
+++ Credentia-Winforms/CustomControls/CustomTextBox.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Credentia_Winforms.CustomControls
+{
+    partial class CustomTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(7, 7);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 18);
+            textBox1.TabIndex = 0;
+            // 
+            // CustomTextBox
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = SystemColors.Window;
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.DimGray;
+            Name = "CustomTextBox";
+            Padding = new Padding(7);
+            Size = new Size(250, 32);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
